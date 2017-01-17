ac465cf1f070a7873051a3d56556d6f542a8003e
H9304 <H9304@ICT-421-WS-31>
2017-01-17 16:17:33 +0200

    Salute done

--- MiiaFirst/Program.cs
+++ MiiaFirst/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// Testing 17.1.2017
+/// </summary>
 namespace MiiaFirst
 {
     class Program
